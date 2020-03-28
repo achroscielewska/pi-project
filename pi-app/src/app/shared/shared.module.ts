4091309a637701629f4199ffb7dca24fed3af3b5
Agnieszka Chróścielewska <agnieszka.chroscielewska@altkomsoftware.pl>
2020-03-28 19:41:00 +0100

    app : assets setup

--- pi-app/src/app/shared/shared.module.ts
+++ pi-app/src/app/shared/shared.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-
+import { ButtonsModule } from 'ngx-bootstrap/buttons';
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    ButtonsModule.forRoot()
+  ],
+  exports: [
+    ButtonsModule
   ]
 })
 export class SharedModule { }
